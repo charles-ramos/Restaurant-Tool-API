46eeb454bd7f388f37f22b144c981237d218d8ea
ekirchner <emma.kirchner@develappers.de>
2023-04-29 15:00:26 +0200

    added controller

---
+++ Restaurant-Tool-API/Restaurant-Tool-API/Controllers/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant_Tool_API.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    [Produces("application/json")]
+    public class MenuController : ControllerBase
+    {
+    }
+}
